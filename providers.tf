2da7c5106bc0a6605c890f3a2ee68db0c5308b01
mdcravero <mdcravero@gmail.com>
2023-06-26 02:21:46 +0000

    hardcode endpoint_url

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@ provider "aws" {
   access_key = "test"
   secret_key = "test"
   region  = var.aws_region
-  s3_use_path_style           = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
+  #  s3_use_path_style           = true
+  #  skip_credentials_validation = true
+  #  skip_metadata_api_check     = true
+  #  skip_requesting_account_id  = true
 }
--- s3_json_dynamodb.py
+++ s3_json_dynamodb.py
@@ -1,19 +1,20 @@
 import boto3
 import json
 import ast
-s3_client = boto3.client('s3')
+s3_client = boto3.client("s3", endpoint_url="http://172.17.0.2:4566")
+#s3_client = boto3.client('s3')
 dynamodb_client = boto3.resource('dynamodb')
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     json_file_name = event['Records'][0]['s3']['object']['key']
-    json_object = s3_client.get_object(Bucket="bucket-json",Key="client_data")
-    #file_reader = json_object.get()['Body'].read().decode("utf-8")
+    json_object = s3_client.get_object(Bucket=bucket,Key=json_file_name)
+    file_reader = json_object['Body'].read().decode("utf-8")
     print("##PRINT")
     print(event)
     print(json_file_name)
     print(bucket)
-    print(json_object)
-#    file_reader = ast.literal_eval(file_reader)
-#    table = dynamodb_client.Table('client_data')
-#    table.put_item(Item=file_reader)
-#    return 'success'
+    print(file_reader)
+    file_reader = ast.literal_eval(file_reader)
+    table = dynamodb_client.Table('client_data')
+    table.put_item(Item=file_reader)
+    return 'success'
